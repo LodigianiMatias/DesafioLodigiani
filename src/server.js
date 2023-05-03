d78515ac0bdeba81920d4e288cb4a32b6a0978fd
Matias <-lodigiani.matias97@gmail.com>
2023-05-02 22:21:21 -0300

    Change PORT to 8080

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-const PORT = 4000
+const PORT = 8080
 
 app.use('/api', apiRouter)
 
