af4c6822e4506555726f3b5fc82323c82bb68fe5
Matias <-lodigiani.matias97@gmail.com>
2023-05-03 22:54:39 -0300

    Add Multer to dependecies

--- src/server.js
+++ src/server.js
@@ -5,10 +5,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
 const PORT = 8080
 
 app.use('/api', apiRouter)
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port http://localhost:${PORT}`)
+  console.log(`Server up and running on port http://localhost:${PORT}`)
 })
