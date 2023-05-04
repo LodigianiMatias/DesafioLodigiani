be0633285d2202a8c73dd6f12c082676cd6e6d96
Matias <-lodigiani.matias97@gmail.com>
2023-05-03 22:02:24 -0300

    Add public folder

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 const PORT = 8080
 
 app.use('/api', apiRouter)
