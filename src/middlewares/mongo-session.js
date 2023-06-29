9f6b47a8cc7f93658670ada63a24b2718e5ac9d8
Matias <-lodigiani.matias97@gmail.com>
2023-06-28 23:33:17 -0300

    Fixing env error

--- src/middlewares/mongo-session.js
+++ src/middlewares/mongo-session.js
@@ -4,7 +4,7 @@ import MongoStore from 'connect-mongo'
 import session from 'express-session'
 
 export const mongoSession = session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.MONGO_SECRET,
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URL,
     autoRemove: 'native',
